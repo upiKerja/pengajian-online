1f9d0367ad44835a4b353ee9f1bf7b0e3077a659
upiKerja <kaplingtumbal@gmail.com>
2025-08-22 14:14:47 +0700

    ininya di iniin

--- server/views/kelas.nim
+++ server/views/kelas.nim
@@ -1,10 +1,33 @@
 import prologue
+import nimja
+import os
+import puppy
+import json
+import strformat
+import strutils
 
 proc kelasIndex*(ctx: Context) {.async.} =
-    await ctx.staticFileResponse("index.html", "src/pages/kelas")
+    resp loadPrologueEnv(".env").get("rijal")
 
 proc kelasPage*(ctx: Context) {.async.} =
-    await ctx.staticFileResponse("page.html", "src/pages/kelas")  
+    proc myRenderProc(title: string, description: string): string =
+        compileTemplateFile("../../src/pages/kelas/page.xhtml", baseDir = getScriptDir())    
+    
+    var
+        slug = ctx.getPathParams("slug")
+        ball = puppy.get(
+            fmt"https://hggxutlouhypvgmrsjkm.supabase.co/rest/v1/kelas?slug=eq.{slug}&select=judul,deskripsi",
+            @[("apikey", loadPrologueEnv(".env").get("SUPABASE_KEY"))]
+        )
+        sukamto = parseJson ball.body
+
+    if sukamto.len > 0:
+        resp myRenderProc(
+            getStr sukamto[0]["judul"],
+            getStr sukamto[0]["deskripsi"])        
+    else :
+        resp "<h1>404 Not Found</h1>"
+
 
 proc kelasDaftar*(ctx: Context) {.async.} =
     resp "Ini Rijal"
