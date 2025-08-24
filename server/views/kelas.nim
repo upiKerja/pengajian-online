ac3d37cb210ba16066cada54bf4286de64f3b68b
upiKerja <kaplingtumbal@gmail.com>
2025-08-24 17:32:41 +0700

    Kelas SSR

--- server/views/kelas.nim
+++ server/views/kelas.nim
@@ -10,21 +10,21 @@ proc kelasIndex*(ctx: Context) {.async.} =
     resp loadPrologueEnv(".env").get("rijal")
 
 proc kelasPage*(ctx: Context) {.async.} =
-    proc myRenderProc(title: string, description: string): string =
+    proc myRenderProc(
+        title: string,
+        description: string): string =
         compileTemplateFile("../../src/pages/kelas/page.xhtml", baseDir = getScriptDir())    
     
     var
         slug = ctx.getPathParams("slug")
         ball = puppy.get(
-            fmt"https://hggxutlouhypvgmrsjkm.supabase.co/rest/v1/kelas?slug=eq.{slug}&select=judul,deskripsi",
-            @[("apikey", loadPrologueEnv(".env").get("SUPABASE_KEY"))]
-        )
+            fmt"http://localhost:8081/kelas/meta/{slug}")
         sukamto = parseJson ball.body
 
-    if sukamto.len > 0:
+    if sukamto["data"].len > 0:
         resp myRenderProc(
-            getStr sukamto[0]["judul"],
-            getStr sukamto[0]["deskripsi"])        
+            getStr sukamto["data"]["judul"],
+            getStr sukamto["data"]["deskripsi"])        
     else :
         resp "<h1>404 Not Found</h1>"
 
