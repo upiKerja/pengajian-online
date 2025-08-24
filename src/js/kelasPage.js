95179fd910abdb1252a677091f505bf3bafcd4ea
upiKerja <kaplingtumbal@gmail.com>
2025-08-24 17:49:47 +0700

    replace .xhtml to .html

--- src/js/kelasPage.js
+++ src/js/kelasPage.js
@@ -1,6 +1,9 @@
-async function fetch_get_data(url) {
+async function fetch_get_data() {
+    // let slug = window.location.href.at(-1)
+    let slug = "akhlak-mulia-meneladani-rasulullah"
+    let url = "http://localhost:8081/kelas/index/" + slug
     try {
-        const response = await axios.get(url());
+        const response = await axios.get(url);
         document.querySelector("#loading").classList.add("hidden")
 
         return response.data
@@ -10,4 +13,16 @@ async function fetch_get_data(url) {
             document.querySelector("body").innerHTML = "<h1>404</h1>"
         }
     }    
+}
+
+async function fetch_pertemuan(id_kelas) {
+    let url = "http://localhost:8081/kelas/index/" + id_kelas
+    try {
+        const response = await axios.get(url)
+        if (response) {
+            return [true, response]
+        } else { return [false] }
+    } catch (error) {
+        return error 
+    }
 }
